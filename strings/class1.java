6cec5fe7223047fdffdb950e7bf197befc9178d5
nahid4942 <nafe.amin.nahid@g.bracu.ac.bd>
2025-06-02 00:54:17 +0600

    1

---
+++ strings/class1.java
@@ -0,0 +1,18 @@
+package strings;
+
+import java.util.Scanner;
+public class class1 {
+
+    
+    public static void main (String[]args)
+    {
+        char arr [][] = {{'a','b','c','d'},{'e','f','g','h'},{'i','j','k','l'},{'m','n','o','p'}};
+        String str = "ABCD";
+        String str2 = new String("xyz");
+
+              Scanner sc = new Scanner (System.in);
+              
+              String name;
+              name = sc.nextLine();
+    }
+}
