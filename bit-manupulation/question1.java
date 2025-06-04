e7dc77a8ded80a57c6df31c8ff2a622dce679be8
nahid4942 <nafe.amin.nahid@g.bracu.ac.bd>
2025-06-05 02:51:09 +0600

    2

---
+++ bit-manupulation/question1.java
@@ -0,0 +1,25 @@
+
+
+public class question1 {
+
+    public static  void oddoreven( int n ){
+        int bitmask = 1;
+        if (( n & bitmask)==0){
+            System.out.println("even")
+        } else {
+            System.out.println("odd");
+        }
+    
+    } 
+
+    public static void main (String[]args){
+        // check if the number is odd or even 
+       
+       
+       
+       
+        // need to check the last bit of numbers 
+    } 
+
+    
+}
